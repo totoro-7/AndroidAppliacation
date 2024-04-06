5d77f32fbd45cdf94a5bf8abeaef6449c2efa85e
totoro-7 <yinyu18.tan@gmail.com>
2024-04-06 19:38:02 +0800

    Sign In, Sign Up Updates

--- Android App/app/src/main/java/com/example/androidappliacation/Login_Page.java
+++ Android App/app/src/main/java/com/example/androidappliacation/Login_Page.java
@@ -1,14 +1,213 @@
 package com.example.androidappliacation;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login_Page extends AppCompatActivity {
+
+    EditText mail;
+    EditText password;
+    Button signIn;
+    SignInButton signInGoogle;
+    TextView signUp;
+
+    GoogleSignInClient googleSignInClient;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        //register
+        registerActivityForGoogleSignIn();
+
+        mail = findViewById(R.id.editTextLoginEmail);
+        password = findViewById(R.id.editTextLoginPassword);
+        signIn = findViewById(R.id.buttonSignIn);
+        signInGoogle = findViewById(R.id.buttonLoginGoogleSignIn);
+        signUp = findViewById(R.id.textViewLoginSignUp);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userEmail = mail.getText().toString();
+                String userPassword = password.getText().toString();
+
+                signInWithFirebase(userEmail,userPassword);
+
+            }
+        });
+
+        signInGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                signinGoogle();
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent (Login_Page.this, Sign_Up_Page.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+    public void signinGoogle(){
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("409318972903")
+                .requestEmail().build();
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso);
+
+        signin();
+    }
+
+    public void signin(){
+
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        activityResultLauncher.launch(signInIntent);
+
+    }
+
+    public void registerActivityForGoogleSignIn() {
+
+        activityResultLauncher
+                = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                        int resultCode = result.getResultCode();
+                        Intent data = result.getData();
+
+                        if (resultCode == RESULT_OK && data != null){
+
+                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                            firebaseSignInWithGoogle(task);
+
+                        }
+
+                    }
+                });
+    }
+
+    private void firebaseSignInWithGoogle(Task<GoogleSignInAccount> task){
+
+
+        try {
+            GoogleSignInAccount account = task.getResult(ApiException.class);
+            Toast.makeText(this,"Successfully signed in",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(Login_Page.this, MainActivity.class);
+            startActivity(i);
+            finish();
+            firebaseGoogleAccount(account);
+
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void firebaseGoogleAccount(GoogleSignInAccount account){
+
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        auth.signInWithCredential(authCredential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            //FirebaseUser user = auth.getCurrentUser();
+
+
+                        }
+                        else {
+
+                        }
+                    }
+                });
+    }
+    public void signInWithFirebase(String userEmail,String userPassword)
+    {
+
+        signIn.setClickable(false);
+
+        auth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            Intent i = new Intent(Login_Page.this, MainActivity.class);
+                            startActivity(i);
+                            finish();
+                            Toast.makeText(Login_Page.this,"Sign in is successful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(Login_Page.this,"Sign in is not successful",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null)
+        {
+            Intent i = new Intent(Login_Page.this,MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+
     }
 }
--- Android App/app/src/main/java/com/example/androidappliacation/MainActivity.java
+++ Android App/app/src/main/java/com/example/androidappliacation/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.androidappliacation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView signOut;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signOut = findViewById(R.id.textViewSignOut);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                auth.signOut();
+                Intent i = new Intent(MainActivity.this,Login_Page.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
 
     }
 }
